c961c7aefef40f60dd7e78683873dafad3d431db
khalid <160763572+Dukemano@users.noreply.github.com>
2024-07-31 15:35:11 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+// Part 1: Data Types
+
+// 1. Create variables of different data types
+const stringVar = "Hello, World!";
+const numberVar = 42;
+const booleanVar = true;
+const nullVar = null;
+const undefinedVar = undefined;
+const objectVar = { name: "John", age: 30 };
+const symbolVar = Symbol("description");
+
+// 2. Print these variables to the console
+console.log("String:", stringVar);
+console.log("Number:", numberVar);
+console.log("Boolean:", booleanVar);
+console.log("Null:", nullVar);
+console.log("Undefined:", undefinedVar);
+console.log("Object:", objectVar);
+console.log("Symbol:", symbolVar);
 
